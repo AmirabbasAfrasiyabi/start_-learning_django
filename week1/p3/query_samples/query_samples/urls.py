310010f22ebbdecce60988cc8c1941789151dd7f
amirabbasafrasiyabi <abb.afrasiyabi@gmail.com>
2024-03-05 19:18:46 +0330

    urls update

--- week1/p3/query_samples/query_samples/urls.py
+++ week1/p3/query_samples/query_samples/urls.py
@@ -19,10 +19,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from posts.views import retrieve_posts , retrieve_posts_exclude_sample
+from posts.views import retrieve_posts , retrieve_posts_exclude_sample ,retrieve_posts_with_equal_content_title
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', retrieve_posts),
     path('posts/exclude/', retrieve_posts_exclude_sample),
+    path('posts/same-title/', retrieve_posts_with_equal_content_title),
 ]
