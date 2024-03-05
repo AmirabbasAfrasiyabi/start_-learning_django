ab8c15dbe5c54b1484a070f12a4aad0fbc03f653
amirabbasafrasiyabi <abb.afrasiyabi@gmail.com>
2024-03-05 15:36:38 +0330

    model(posts updates)

--- week1/p3/query_samples/posts/models.py
+++ week1/p3/query_samples/posts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+
+User = get_user_model()
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
