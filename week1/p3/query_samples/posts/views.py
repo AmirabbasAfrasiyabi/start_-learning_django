90b693dd3fc306effb7e50b22077db64f3c74a75
amirabbasafrasiyabi <abb.afrasiyabi@gmail.com>
2024-03-05 16:02:29 +0330

    veiw for posts update

--- week1/p3/query_samples/posts/views.py
+++ week1/p3/query_samples/posts/views.py
@@ -1,3 +1,24 @@
+from datetime import datetime, timedelta
+
+from django.db.models import CharField
+from django.db.models import Q, F, When, Case
+from django.http import HttpResponse
+# Create your views here.
 from django.shortcuts import render
 
-# Create your views here.
+# from posts.forms import post_formset, book_formset
+from posts.models import Post, Comment, PostTemplate, Book
+
+def retrieve_posts(request):
+    filter_exp = Q()
+
+    if 'title' in request.GET.keys():
+        filter_exp &= Q(title__in=request.GET.getlist('title'))
+
+    if 'content' in request.GET.keys():
+        filter_exp |= Q(content=request.GET.get('content'))
+
+    posts = Post.objects.filter(~filter_exp)
+    # is equal to: posts = Post.objects.exclude(filter_exp)
+    message = f'post count: {posts.count()}'
+    return HttpResponse()
